cff1c2fd870005237916bc342ece67247fbd7c87
2400039067 <karrisrilakshmi2006@gmail.com>
2025-09-27 10:58:51 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,6 @@
+function greet(name)
+{
+    console.log("HELLO,"+name);
+}
+greet("Durga");
+greet("Dinisha");
---
+++ sample1.js
@@ -0,0 +1,8 @@
+let age=18;
+if(age>=18)
+{
+    console.log("you are elibile to vote");
+}
+else{
+    console.log("you are not eligible to vote");
+}
